c6f4d1acb37158dae4f8a8c7403a93c44985dd8d
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2014-04-23 08:45:58 -0300

    placeholder file for Provisioning WHMCS Module

---
+++ modules/servers/nic_ar/nic_ar.php
@@ -0,0 +1,10 @@
+<?php
+#
+# NIC.ar Provisioning WHMCS Module v0.1
+#
+# A WHMCS provisioning module for the nic!alert API to NIC.ar
+# https://github.com/vivaserver/whmcs-nic_ar
+#
+# (c)2014 Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
+#
+# ref. http://docs.whmcs.com/Configuring_Products/Services#Module_Settings_.28aka_Provisioning.29
