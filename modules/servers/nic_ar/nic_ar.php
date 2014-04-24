386d750a095b2b9ac749d68d089e6220ec1dc1d8
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2014-04-23 23:34:37 -0300

    skeleton Provisioning Module

--- modules/servers/nic_ar/nic_ar.php
+++ modules/servers/nic_ar/nic_ar.php
@@ -8,3 +8,16 @@
 # (c)2014 Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
 #
 # ref. http://docs.whmcs.com/Configuring_Products/Services#Module_Settings_.28aka_Provisioning.29
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+
+function nic_ar_ConfigOptions() {
+  $config = array(
+    "token" => array('FriendlyName'=>'Token',"Type"=>"text", "Description"=>"<a href='http://api.nicalert.me/docs#sp4' target='_blank'>API Token</a>")  # configoption1
+  );
+  return $config;
+}
+
+function nic_ar_CreateAccount($params) {
+  # TODO
+}
